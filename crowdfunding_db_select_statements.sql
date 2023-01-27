d4e91d10fd33354ff77f73a2603b9d7738691725
Nou Yang <nouyang0620@gmail.com>
2023-01-26 21:11:41 -0600

    ERD and DB

---
+++ crowdfunding_db_select_statements.sql
@@ -0,0 +1,12 @@
+-- Select statements to verify tables were imported with the correct data
+select *
+from category;
+
+select *
+from subcategory;
+
+select * 
+from contacts;
+
+select *
+from campaign;
